41fa1cb3271c59dd99b5fb29edacc36e9d2af76c
delphyinium <tiger.roar@live.com>
2024-02-11 01:58:03 -0500

    small typo correction

--- src/preload/index.ts
+++ src/preload/index.ts
@@ -1,5 +1,5 @@
 import { CreateNote, DeleteNote, GetNotes, ReadNote, WriteNote } from '@shared/types'
-import { contextBridge, ipcRendered } from 'electron'
+import { contextBridge, ipcRenderer } from 'electron'
 
 if (!process.contextIsolated) {
   throw new Error('contextIsolation must be enabled in the BrowserWindow')
@@ -8,12 +8,12 @@ if (!process.contextIsolated) {
 try {
   contextBridge.exposeInMainWorld('context', {
     locale: navigator.language,
-    getNotes: (...args: Parameters<GetNotes>) => ipcRendered.invoke('get-notes', ...args),
-    readNote: (...args: Parameters<ReadNote>) => ipcRendered.invoke('read-note', ...args),
-    writeNote: (...args: Parameters<WriteNote>) => ipcRendered.invoke('write-note', ...args),
-    createNote: (...args: Parameters<CreateNote>) => ipcRendered.invoke('create-note', ...args),
-    deleteNote: (...args: Parameters<DeleteNote>) => ipcRendered.invoke('delete-note', ...args)
-})
+    getNotes: (...args: Parameters<GetNotes>) => ipcRenderer.invoke('getNotes', ...args),
+    readNote: (...args: Parameters<ReadNote>) => ipcRenderer.invoke('readNote', ...args),
+    writeNote: (...args: Parameters<WriteNote>) => ipcRenderer.invoke('writeNote', ...args),
+    createNote: (...args: Parameters<CreateNote>) => ipcRenderer.invoke('createNote', ...args),
+    deleteNote: (...args: Parameters<DeleteNote>) => ipcRenderer.invoke('deleteNote', ...args)
+  })
 } catch (error) {
   console.error(error)
 }
